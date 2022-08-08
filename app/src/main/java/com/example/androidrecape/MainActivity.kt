98755a9a893b9a0d7ea81defa1fb17a49326863f
Zohaib Akram <zohaib.bin.akram@gmail.com>
2022-08-08 23:23:49 +0500

    add basic location

--- app/src/main/java/com/example/androidrecape/MainActivity.kt
+++ app/src/main/java/com/example/androidrecape/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.androidrecape
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidrecape.databinding.ActivityMainBinding
+import com.example.androidrecape.location.LocationActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        bindingClickListener()
+    }
+
+    private fun bindingClickListener()
+    {
+        binding.locationExampleButton.setOnClickListener {
+            startActivity(LocationActivity.getLaunchIntent(this))
+        }
     }
 }
--- app/src/main/java/com/example/androidrecape/location/LocationActivity.kt
+++ app/src/main/java/com/example/androidrecape/location/LocationActivity.kt
@@ -1,16 +1,100 @@
 package com.example.androidrecape.location
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
-import com.example.androidrecape.R
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.androidrecape.databinding.ActivityLocationBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class LocationActivity : AppCompatActivity() {
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var binding: ActivityLocationBinding
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_location)
+        binding = ActivityLocationBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setupClient();
+        bindingClickListener()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun bindingClickListener() {
+        binding.buttonLastLocation.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    // You can use the API that requires the permission.
+                    getLastLocation()
+                }
+                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected. In this UI,
+                // include a "cancel" or "no thanks" button that allows the user to
+                // continue using your app without granting the permission.
+                //showInContextUI(...)
+                    Toast.makeText(this, "We need location dear", Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+        }
+    }
+
+    // Register the permissions callback, which handles the user's response to the
+// system permissions dialog. Save the return value, an instance of
+// ActivityResultLauncher. You can use either a val, as shown in this snippet,
+// or a lateinit var in your onAttach() or onCreate() method.
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+                getLastLocation()
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+            }
+        }
+
+
+    private fun setupClient() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation(){
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                // Got last known location. In some rare situations this can be null.
+                binding.locationTextView.text = "${location?.longitude} , ${location?.latitude}"
+            }
     }
 
     companion object {
