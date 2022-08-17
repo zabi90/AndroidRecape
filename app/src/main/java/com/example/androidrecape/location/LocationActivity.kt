e6f4981f5c57209f2101f35d414f8fea025e3815
Zohaib Akram <zohaib.bin.akram@gmail.com>
2022-08-17 16:39:28 +0500

    localtion updates

--- app/src/main/java/com/example/androidrecape/location/LocationActivity.kt
+++ app/src/main/java/com/example/androidrecape/location/LocationActivity.kt
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
@@ -21,9 +22,15 @@ import com.google.android.gms.location.*
 import com.google.android.gms.tasks.Task
 
 class LocationActivity : AppCompatActivity() {
-    val REQUEST_CHECK_SETTINGS = 121
+    private var requestingLocationUpdates = false
+    private var isLocationPermissonAccess = false
+    private val REQUEST_CHECK_SETTINGS = 121
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest : LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
     lateinit var binding: ActivityLocationBinding
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,36 +38,91 @@ class LocationActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        setupClient();
+        setupClient()
         bindingClickListener()
-        createLocationRequest()
+        getLocationPermission()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations){
+                    // Update UI with location data
+                    // ...
+
+                }
+
+                binding.locationTextView.text = "${locationResult.lastLocation.longitude} , ${locationResult.lastLocation.latitude}"
+            }
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onResume() {
+        super.onResume()
+        if (requestingLocationUpdates) startLocationUpdates()
+    }
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper())
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun bindingClickListener() {
         binding.buttonLastLocation.setOnClickListener {
-            when {
-                ContextCompat.checkSelfPermission(
+            if(isLocationPermissonAccess){
+                getLastLocation()
+            }
+        }
+
+        binding.locationUpdateButton.setOnClickListener {
+            if(isLocationPermissonAccess){
+                createLocationRequest()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun getLocationPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
                     this,
                     Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED -> {
-                    // You can use the API that requires the permission.
-                    getLastLocation()
-                }
-                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                isLocationPermissonAccess = true
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
                 // In an educational UI, explain to the user why your app requires this
                 // permission for a specific feature to behave as expected. In this UI,
                 // include a "cancel" or "no thanks" button that allows the user to
                 // continue using your app without granting the permission.
                 //showInContextUI(...)
-                    Toast.makeText(this, "We need location dear", Toast.LENGTH_SHORT).show()
-                }
-                else -> {
-                    // You can directly ask for the permission.
-                    // The registered ActivityResultCallback gets the result of this request.
-                    requestPermissionLauncher.launch(
+                Toast.makeText(this, "We need location dear", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissionLauncher.launch(
                         Manifest.permission.ACCESS_FINE_LOCATION)
-                }
             }
         }
     }
@@ -71,43 +133,45 @@ class LocationActivity : AppCompatActivity() {
 // or a lateinit var in your onAttach() or onCreate() method.
 
     private val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // Permission is granted. Continue the action or workflow in your
-                // app.
-                getLastLocation()
-            } else {
-                // Explain to the user that the feature is unavailable because the
-                // features requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
+            registerForActivityResult(
+                    ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                    isLocationPermissonAccess = true
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // features requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
             }
-        }
 
 
     private fun setupClient() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
-    @SuppressLint("MissingPermission")
-    private fun getLastLocation(){
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                // Got last known location. In some rare situations this can be null.
-                binding.locationTextView.text = "${location?.longitude} , ${location?.latitude}"
-            }
-    }
 
-    fun createLocationRequest() {
-        val locationRequest = LocationRequest.create().apply {
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    binding.locationTextView.text = "${location?.longitude} , ${location?.latitude}"
+                }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun createLocationRequest() {
+
+         locationRequest = LocationRequest.create().apply {
             interval = 10000
             fastestInterval = 5000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
         val builder = LocationSettingsRequest.Builder()
-            .addLocationRequest(locationRequest)
+                .addLocationRequest(locationRequest)
 
         val client: SettingsClient = LocationServices.getSettingsClient(this)
         val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
@@ -117,11 +181,13 @@ class LocationActivity : AppCompatActivity() {
             // location requests here.
             // ...
             Toast.makeText(this@LocationActivity, "Setting On", Toast.LENGTH_SHORT).show()
+            requestingLocationUpdates = true
+            startLocationUpdates()
 
         }
 
         task.addOnFailureListener { exception ->
-            if (exception is ResolvableApiException){
+            if (exception is ResolvableApiException) {
                 // Location settings are not satisfied, but this can be fixed
                 // by showing the user a dialog.
                 try {
@@ -132,15 +198,18 @@ class LocationActivity : AppCompatActivity() {
                             REQUEST_CHECK_SETTINGS)
                 } catch (sendEx: IntentSender.SendIntentException) {
                     // Ignore the error.
+                    requestingLocationUpdates = false
                 }
             }
         }
     }
-
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == REQUEST_CHECK_SETTINGS && resultCode == RESULT_OK){
+        if (requestCode == REQUEST_CHECK_SETTINGS && resultCode == RESULT_OK) {
             Toast.makeText(this@LocationActivity, "Setting On from result", Toast.LENGTH_SHORT).show()
+            requestingLocationUpdates = true
+            startLocationUpdates()
         }
     }
 
