6b9128142be702906994e7a9ad07c49bc46f55bb
Zohaib Akram <zohaib.bin.akram@gmail.com>
2022-10-26 18:45:57 +0500

    Job Schedulers

---
+++ app/src/main/java/com/example/androidrecape/schedulers/MyJobService.kt
@@ -0,0 +1,50 @@
+package com.example.androidrecape.schedulers
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.util.Log
+
+class MyJobService : JobService() {
+
+    private var isJobCancelled = false
+    override fun onStartJob(params: JobParameters?): Boolean {
+        //This is also work on main thread of application
+        //you have to create your own background thread
+        doBackgroundWork(params)
+       return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        isJobCancelled = true
+        Log.v(TAG,"Job cancelled")
+       return false
+    }
+
+    private fun doBackgroundWork(params: JobParameters?){
+        Log.v(TAG,"Job Started")
+        object :Thread(){
+            override fun run() {
+                super.run()
+
+                for (i in 10 downTo 0){
+
+                    if(isJobCancelled){
+                        return
+                    }
+                    try {
+                        Log.v(TAG,"Job work at $i")
+                        sleep(1000)
+                    }catch (ex :Exception){
+                        Log.v(TAG,"error while running job")
+                    }
+                    jobFinished(params,false)
+                }
+            }
+        }.start()
+    }
+
+    companion object{
+        private const val TAG = "MyJobService"
+        public const val JOB_ID = 31
+    }
+}
